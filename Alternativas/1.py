81cab71c44b4cd968873d18b1007feb385c8e0d5
AlbertoRS05 <albertoromerosantana05@gmail.com>
2025-02-20 22:36:42 +0000

    Create 1.py

---
+++ Alternativas/1.py
@@ -0,0 +1,8 @@
+# Algoritmo que pida dos números e indique si el primero es mayor que el segundo.
+num1=int(input("Diga un número: "))
+num2=int(input("Diga otro número: "))
+
+if num1 > num2:
+    print("El num1 es mayor que el num2")
+else:
+    print("El num2 es mayor que el num1")
